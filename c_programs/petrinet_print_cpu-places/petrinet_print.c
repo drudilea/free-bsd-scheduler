8558bb18bb46cf29e4c16856632ee4099bda0b75
Nicolas Goldman <nicolasgoldman07@gmail.com>
2022-04-20 07:08:56 -0300

    Add kernel module to print cpu places

---
+++ c_programs/petrinet_print_cpu-places/petrinet_print.c
@@ -0,0 +1,34 @@
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/param.h>
+#include <sys/sched_petri.h>
+#include <sys/systm.h>
+
+/* The function called at load/unload. */
+static int event_handler(struct module *module, int event, void *arg) {
+  int e = 0; /* Error, 0 for normal return status */
+  switch (event) {
+  case MOD_LOAD:
+    uprintf("Hello chicos tesis! \n");
+    print_cpu_places();
+    break;
+  case MOD_UNLOAD:
+    uprintf("Bye Bye chicos tesis !\n");
+    break;
+  default:
+    e = EOPNOTSUPP; /* Error, Operation Not Supported */
+    break;
+  }
+
+  return (e);
+}
+
+/* The second argument of DECLARE_MODULE. */
+static moduledata_t petrinet_print_conf = {
+    "petrinet_print", /* module name */
+    event_handler,    /* event handler */
+    NULL              /* extra data */
+};
+/* TODO: Test SI_SUB_EXEC as third argument */
+DECLARE_MODULE(petrinet_print, petrinet_print_conf, SI_SUB_DRIVERS,
+               SI_ORDER_MIDDLE);
