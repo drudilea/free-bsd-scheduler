918701e1d2cc7d3b087a86c6adf2b16ed6d21f1e
Leandro Drudi <drudilea@hotmail.com>
2022-04-25 11:33:49 -0300

    Reorder of #includes and clang-format config

--- c_programs/HelloWorld_KernelModule/hello.c
+++ c_programs/HelloWorld_KernelModule/hello.c
@@ -1,32 +1,32 @@
+// clang-format off
 #include <sys/param.h>
 #include <sys/module.h>
-#include <sys/kernel.h>
 #include <sys/systm.h>
+#include <sys/kernel.h>
+// clang-format on
 
 void hello_debug_unload(void);
 void hello_debug_unload(void) /* needed for testing remote debugging */
 {
-    uprintf("Bye Bye hello_debug_unload\n");
+  uprintf("Bye Bye hello_debug_unload\n");
 }
 
 /* The function called at load/unload. */
-static int event_handler(struct module *module, int event, void *arg)
-{
-    int e = 0; /* Error, 0 for normal return status */
-    switch (event)
-    {
-    case MOD_LOAD:
-        uprintf("Hello chicos tesis! \n");
-        break;
-    case MOD_UNLOAD:
-        uprintf("Bye Bye chicos tesis !\n");
-        break;
-    default:
-        e = EOPNOTSUPP; /* Error, Operation Not Supported */
-        break;
-    }
+static int event_handler(struct module *module, int event, void *arg) {
+  int e = 0; /* Error, 0 for normal return status */
+  switch (event) {
+  case MOD_LOAD:
+    uprintf("Hello chicos tesis! \n");
+    break;
+  case MOD_UNLOAD:
+    uprintf("Bye Bye chicos tesis !\n");
+    break;
+  default:
+    e = EOPNOTSUPP; /* Error, Operation Not Supported */
+    break;
+  }
 
-    return (e);
+  return (e);
 }
 /* The second argument of DECLARE_MODULE. */
 static moduledata_t hello_conf = {
