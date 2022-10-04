e669ceb11d78754285a602c3f4be281d38aae689
Leandro Drudi <drudilea@hotmail.com>
2022-10-04 10:18:44 -0300

    New C programs to get 100% CPU usage

---
+++ c_programs/parallel_calc_OMP/parallel_calc_OMP.c
@@ -0,0 +1,33 @@
+#include <omp.h>
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+  double start, end;
+  double runTime;
+  start = omp_get_wtime();
+  int num = 1, primes = 0;
+
+  int limit = 1000000;
+
+#pragma omp parallel for schedule(dynamic) reduction(+ : primes)
+  for (num = 1; num <= limit; num++) {
+    int i = 2;
+    while (i <= num) {
+      if (num % i == 0)
+        break;
+      i++;
+    }
+    if (i == num)
+      primes++;
+    //      printf("%d prime numbers calculated\n",primes);
+  }
+
+  end = omp_get_wtime();
+  runTime = end - start;
+  printf(
+      "This machine calculated all %d prime numbers under %d in %g seconds\n",
+      primes, limit, runTime);
+
+  return 0;
+}
