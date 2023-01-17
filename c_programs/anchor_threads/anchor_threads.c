97bf8c33c0d6b4522dc68d6659c2aa731cf79329
Nico Goldman <ngoldman@tangocode.com>
2023-01-17 11:06:49 -0300

    Add anchor_threads module to enable thread pinning in cpus

---
+++ c_programs/anchor_threads/anchor_threads.c
@@ -0,0 +1,39 @@
+// clang-format off
+#include <sys/param.h>
+#include <sys/module.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/sched_petri.h>
+// clang-format on
+
+/* The function called at load/unload. */
+static int event_handler(struct module *module, int event, void *arg)
+{
+  int e = 0; /* Error, 0 for normal return status */
+  switch (event)
+  {
+  case MOD_LOAD:
+    uprintf("Hello chicos tesis! \n");
+    pin_thread_to_cpu(0);
+    set_print_transition(1000);
+    break;
+  case MOD_UNLOAD:
+    uprintf("Bye Bye chicos tesis !\n");
+    break;
+  default:
+    e = EOPNOTSUPP; /* Error, Operation Not Supported */
+    break;
+  }
+
+  return (e);
+}
+
+/* The second argument of DECLARE_MODULE. */
+static moduledata_t pin_thread_to_cpu_conf = {
+    "pin_thread_to_cpu", /* module name */
+    event_handler,       /* event handler */
+    NULL                 /* extra data */
+};
+/* TODO: Test SI_SUB_EXEC as third argument */
+DECLARE_MODULE(pin_thread_to_cpu, pin_thread_to_cpu_conf, SI_SUB_DRIVERS,
+               SI_ORDER_MIDDLE);
